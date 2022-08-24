cd36b4ad58e298ab66e5068e7f1e9f8a1f19015f
Dagus51 <sdagus46@gmail.com>
2022-08-24 16:30:15 +0200

    cleared directories in computer

--- random_password.py
+++ random_password.py
@@ -1,3 +1,11 @@
 import random
 
-pass_length = input('choose ')
+pass_length = input('choose length of Your new password: ')
+pass_length = int(pass_length)
+password = ""
+
+for i in range(pass_length):
+
+    password += chr(random.randint(33,126))
+
+print(password)
