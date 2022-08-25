cebf071643e018b86c12b2ca9e5d1a9cdd30b277
Dagus51 <sdagus46@gmail.com>
2022-08-25 12:15:06 +0200

    Added player name, deleted unnecessary lines and made game limit to 3 rounds

--- rock.py
+++ rock.py
@@ -1,44 +1,42 @@
 import random
 
+player = input('Player name: ')
+
 choices = ["rock", "paper", "scissors"]
 computer_points = 0
 player_points = 0
 
 while True:
-
     computer_choice = random.choice(choices)
-
     player_choice = input("Rock, Paper or Scissors?:").lower()
-
     print("Computer picks " + computer_choice)
 
     if player_choice == computer_choice:
         print("Draw!")
     elif player_choice == "rock":
         if computer_choice == "paper":
-            print("Computer wins!")
             computer_points += 1
         else:
-            print("Player wins!")
             player_points += 1
     elif player_choice == "paper":
         if computer_choice == "scissors":
-            print("Computer wins!")
             computer_points += 1
         else:
-            print("Player wins!")
             player_points += 1
     elif player_choice == "scissors":
         if computer_choice == "rock":
-            print("Computer wins!")
             computer_points += 1
         else:
-            print("Player wins!")
             player_points += 1
     elif player_choice == "":
         break
     else:
-        print("Please pick a valid choice")
-
-    print("Player score:", player_points)
+        print(player + " please pick a valid choice")
+    if player_points == 3:
+        print('Whole game win ' + player + ' !!!')
+        break
+    if computer_points == 3:
+        print('Whole game win computer !!!')
+        break
+    print(player + " score:", player_points)
     print("Computer score:", computer_points)
